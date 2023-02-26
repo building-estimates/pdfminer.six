989b94f6c62505ab286d272d26f1aeb4f5647bd8
Stephen Thomas <stephen.thomas@buildingestimates.com>
2023-02-27 11:05:12 +1300

    __init__.py should use same version as conda build

--- pdfminer/__init__.py
+++ pdfminer/__init__.py
@@ -1,4 +1,6 @@
-__version__ = "__VERSION__"  # auto replaced with tag in github actions
+import os
+
+__version__ = os.environ.get('GIT_DESCRIBE_TAG', 'v0.0').replace('v', '') + '.' + os.environ.get('GIT_DESCRIBE_NUMBER', '0')
 
 if __name__ == "__main__":
     print(__version__)
